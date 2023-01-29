a3a42a175d7dab8bf208811fc716b62270de9eb0
Yahya Kassab <ya7yakassab@gmail.com>
2023-01-29 03:28:02 +0200

    messiLasgt

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + messi</h1>
+      <h1>Vite + messilast</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
